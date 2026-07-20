b70ac59276459f78007bd2a6e4aaba2bb81f1e0d
mamo-u <mamo.jammy@gmail.com>
2026-07-20 21:33:08 +0900

    v3 update

--- js/camera.js
+++ js/camera.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let stream = null;
+let track = null;
 
 export async function initializeCamera() {
 
@@ -20,17 +21,17 @@ export async function initializeCamera() {
 
         stream = await navigator.mediaDevices.getUserMedia({
 
-            video: {
-                facingMode: "environment"
-            },
+            video: true,
 
             audio: false
 
         });
 
-        video.srcObject = stream;
+        track = stream.getVideoTracks()[0];
 
+        video.srcObject = stream;
         console.log("Camera started");
+        updateDebug();
 
     }
     catch (err) {
@@ -48,5 +49,47 @@ export function stopCamera() {
     stream.getTracks().forEach(track => track.stop());
 
     stream = null;
+
+}
+
+export function updateDebug() {
+
+    if (!track) return;
+
+    const debug = {
+
+        capabilities:
+
+            track.getCapabilities(),
+
+        settings:
+
+            track.getSettings(),
+
+        constraints:
+
+            track.getConstraints(),
+
+        userAgent:
+
+            navigator.userAgent,
+
+        standalone:
+
+            window.matchMedia("(display-mode: standalone)").matches,
+
+        visibility:
+
+            document.visibilityState,
+
+        secureContext:
+
+            window.isSecureContext
+
+    };
+
+    document.getElementById("debugOutput").textContent =
+
+        JSON.stringify(debug, null, 4);
 
 }
